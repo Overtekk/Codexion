3f65dbe8307682e51dab789fd40c6f802f7aa9d1
roandrie <roandrie@student.42lehavre.fr>
2026-02-17 12:44:26 +0100

    debug fifo scheduler

--- coders/simulation/launch.c
+++ coders/simulation/launch.c
@@ -6,7 +6,7 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 15:37:16 by roandrie          #+#    #+#             */
-/*   Updated: 2026/02/17 12:11:23 by roandrie         ###   ########.fr       */
+/*   Updated: 2026/02/17 12:43:47 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,26 @@ void	*start_simulation(void *arg)
 		{
 			if (data->coder[index].have_finished == 1)
 				finish++;
-			else if ((get_time_ms() - data->coder[index].time_bournout) > data->burnout_max)
+			else if ((get_time_ms() - get_burnout(&data->coder[index])) > data->burnout_max)
 			{
 				pthread_mutex_lock(&data->mutex_simu);
 				data->simulation_active = 0;
-				pthread_cond_broadcast(&data->queue_control.cond);
 				pthread_mutex_unlock(&data->mutex_simu);
+				pthread_cond_broadcast(&data->queue_control.cond);
 				print_logs(data->coder[index].id, 0, ACT_BURNS, data);
 				return (NULL);
 			}
-			if (finish == data->nbr_coders)
-			{
-				data->simulation_active = 0;
-				printf(LOG_SUCCESS "\n");
-			}
 			else
 				index++;
+		}
+		if (finish == data->nbr_coders)
+		{
+			pthread_mutex_lock(&data->mutex_simu);
+			data->simulation_active = 0;
+			pthread_mutex_unlock(&data->mutex_simu);
+			pthread_cond_broadcast(&data->queue_control.cond);
+			printf(LOG_SUCCESS "\n");
+			return (NULL);
 		}
 		usleep(300);
 	}
--- coders/simulation/scheduler.c
+++ coders/simulation/scheduler.c
@@ -6,7 +6,7 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 09:30:39 by roandrie          #+#    #+#             */
-/*   Updated: 2026/02/17 12:09:09 by roandrie         ###   ########.fr       */
+/*   Updated: 2026/02/17 12:41:17 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,17 @@ int	scheduler_fifo(t_data *data, t_coder *coder, char *action)
 	if (strcmp(action, ADD_QUEUE) == 0)
 	{
 		add_to_queue(&data->queue_control, coder);
-		while (data->queue_control.first->coder != coder || take_dongle(coder) == 1)
-			pthread_cond_wait(&data->queue_control.cond, &data->queue_control.lock);
+		while (get_simulation(data) == 1 && (data->queue_control.first->coder != coder || take_dongle(coder) == 1))
+		{
+			if (get_simulation(data) == 1 && data->queue_control.first->coder == coder)
+			{
+				pthread_mutex_unlock(&data->queue_control.lock);
+				usleep(1000);
+				pthread_mutex_lock(&data->queue_control.lock);
+			}
+			else
+				pthread_cond_wait(&data->queue_control.cond, &data->queue_control.lock);
+		}
 	}
 	else if (strcmp(action, REMOVE_QUEUE) == 0)
 	{
