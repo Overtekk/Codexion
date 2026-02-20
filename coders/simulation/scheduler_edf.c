9b2ff517151f754a3e367667f3f4c2cb93613b48
Overtek <theovertekgaming@gmail.com>
2026-02-20 20:10:48 +0100

    problem solved

--- coders/simulation/scheduler_edf.c
+++ coders/simulation/scheduler_edf.c
@@ -6,7 +6,7 @@
 /*   By: roandrie <roandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 10:40:41 by roandrie          #+#    #+#             */
-/*   Updated: 2026/02/20 18:46:56 by roandrie         ###   ########.fr       */
+/*   Updated: 2026/02/20 19:09:39 by roandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,14 @@ void	scheduler_edf_add(t_data *data, t_coder *coder)
 	pthread_mutex_lock(&controler->lock);
 	heap_push(controler, coder);
 	pthread_cond_broadcast(&controler->cond);
+	pthread_mutex_unlock(&controler->lock);
 	while (get_simulation(data) == 1)
 	{
 		if (take_dongle(coder) == 0)
 			break ;
-		if (get_simulation(data) == 1 && controler->tree[0] == coder)
-		{
-			pthread_mutex_unlock(&controler->lock);
-			usleep(200);
-			pthread_mutex_lock(&controler->lock);
-		}
-		else
-			pthread_cond_wait(&controler->cond, &controler->lock);
+		usleep(500);
 	}
+	pthread_mutex_lock(&controler->lock);
 	heap_pop(controler, coder);
 	pthread_cond_broadcast(&controler->cond);
 	pthread_mutex_unlock(&controler->lock);
